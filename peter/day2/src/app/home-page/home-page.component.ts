4548d3d664fc2f611234dd6d873ddb67282e90cb
Peter Aigner <mastapetz@icloud.com>
2022-06-14 11:41:06 +0200

    button, array, ngIf ngFor

--- peter/day2/src/app/home-page/home-page.component.ts
+++ peter/day2/src/app/home-page/home-page.component.ts
@@ -6,7 +6,20 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./home-page.component.scss']
 })
 export class HomePageComponent implements OnInit {
-
+  cars: Array<{maker: string, type:string, price: number, kmCount: number}> =[
+    {maker: "VW",
+     type: "Golf",
+    price: 14000,
+    kmCount: 1000},
+    {maker: "VW",
+     type: "Polo",
+    price: 9000,
+    kmCount: 80000},
+    {maker: "Nissan",
+     type: "Skyline",
+    price: 32000,
+    kmCount: 0}
+  ];
   constructor() { }
 
   ngOnInit(): void {
--- peter/day2/src/app/navbar/navbar.component.ts
+++ peter/day2/src/app/navbar/navbar.component.ts
@@ -6,9 +6,16 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./navbar.component.scss']
 })
 export class NavbarComponent implements OnInit {
-
+  title: string ="Car Rental"
+  lableBtn: string ="Donate 10 €"
+  clickCounter :number = 0;
   constructor() { }
+  countClick(){
 
+    this.clickCounter +=10;
+    console.log(this.clickCounter);
+
+}
   ngOnInit(): void {
   }
 
